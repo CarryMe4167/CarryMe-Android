c6f777efdef4e15dfd5bbb1da180f371adc78a50
Fayad Chowdhury <fayad.chowdhury@gmail.com>
2019-10-14 04:52:45 +0600

    Remove MapData class since we no longer need it.

--- app/src/main/java/com/carryme4167/carryme/MapData.kt
+++
@@ -1,9 +0,0 @@
-package com.carryme4167.carryme
-
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
-class MapData(val lat: Double, val long: Double): Parcelable {
-    constructor():this(0.0, 0.0)
-}
